c8fc15b2bb3a1fdb6f65c905c9038ea3a93159f3
haibinh127 <binhnguyen127@gmail.com>
2021-01-22 16:41:04 +0700

    yes/no

--- vote-yes-no/public/home/index.js
+++ vote-yes-no/public/home/index.js
@@ -1,4 +1,5 @@
-const question = document.getElementById('randomQuestion')
-fetch('http://localhost:8080/random-question')
-.then(res => res.json())
-.then(res => question.innerHTML = res.content.content);
+let question = document.getElementById('randomQuestion');
+let randomQuestion = fetch('http://localhost:8080/random-question');
+randomQuestion
+    .then(res => res.json())
+    .then(res => question.innerHTML = (res.data.content)+" ?") 
--- vote-yes-no/server.js
+++ vote-yes-no/server.js
@@ -2,7 +2,17 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const path = require('path');
-
+const getRandomQuestion = (request, response) => {
+    const data = JSON.parse(
+        fs.readFileSync(path.resolve(__dirname, "./data.json"))
+    )
+    const id = Math.floor(Math.random() * data.length) + 1;
+    const randomQuestion = data[id]
+    response.send({
+        success: true,
+        data: randomQuestion,
+    })
+}
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
@@ -13,44 +23,11 @@ app.get('ask', (req, res) => {
     res.sendFile(path.resolve(__dirname, './public/ask/index.html'));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './public/404/index.html'));
-});
-
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './public/home/index.html'))
 });
 
-app.post('/random-question', (req, res) => {
-
-    let data;
-    try {
-        data = JSON.parse(fs.readFileSync('data.json'));
-        console.log(data);
-    } catch (err) {
-        data = [];
-    }
-    const randomQuestion = data[Math.floor(Math.random() * data.length) + 1];
-    res.send({
-        success: 1,
-        content: randomQuestion,
-    });
-
-    // const data = require('./data.json')
-    // const randomQuestion = {
-    //     _id: [Math.floor(Math.random() * data.length) + 1],
-    //     content: req.body.content,
-    //     yes: 0,
-    //     no: 0,
-    // }
-    // const newRandomQuestion = [...data, randomQuestion];
-    // fs.writeFileSync('data.json', JSON.stringify(newRandomQuestion))
-    // res.send({
-    //     success: 1,
-    //     data: randomQuestion,
-    // }
-    // );
-});
+app.get('/random-question', getRandomQuestion);
 app.post('/create-question', (req, res) => {
     const data = require('./data.json');
     const newQuestion = {
@@ -65,6 +42,10 @@ app.post('/create-question', (req, res) => {
         success: 1,
         data: newQuestion
     });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './public/404/index.html'));
 });
 
 app.listen(8080, (err) => {
